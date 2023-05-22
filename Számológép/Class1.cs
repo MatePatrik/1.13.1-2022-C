44fe5747925244a8c42ad49364a5b90eaaf9c7c6
MatePatrik <112942096+MatePatrik@users.noreply.github.com>
2023-05-22 11:32:56 +0200

    Add files via upload

---
+++ Számológép/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzámológépOOP
+{
+    internal class Számológép
+    {
+        public char muvelet;
+        public Számológép() { }
+        public void JEL(char muvelet)
+        {
+            this.muvelet = muvelet;
+        }
+        public void SZAMOL(float szam, float szam2)
+        {
+            if (this.muvelet == '+')
+            {
+                szam += szam2;
+                Console.WriteLine(szam);
+            }
+            else if (this.muvelet == '-')
+            {
+                szam -= szam2;
+                Console.WriteLine(szam);
+            }
+            else if (this.muvelet == '*')
+            {
+                szam *= szam2;
+                Console.WriteLine(szam);
+            }
+            else if (this.muvelet == '/')
+            {
+                szam /= szam2;
+                Console.WriteLine(szam);
+            }
+            else
+            {
+                Console.WriteLine("Hibás bemenet!");
+            }
+        }
+    }
+}
---
+++ Számológép/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzámológépOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam;
+            int szam2;
+            char muvelet;
+            bool a = true;
+            Számológép C1 = new Számológép();
+            while (a) 
+            {
+                try
+                {
+                    Console.WriteLine("Add meg a számot: ");
+                    szam = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Add meg a másik számot: ");
+                    szam2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Add meg a műveletet: ");
+                    muvelet = char.Parse(Console.ReadLine());
+                    C1.JEL(muvelet);
+                    C1.SZAMOL(szam, szam2);
+                }
+                catch
+                {
+                    Console.WriteLine("Hibás bemenet!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
